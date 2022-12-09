0a7c752f21f20d2b81f97fb7c8b524a81873c0f4
EdgarPerezArce <118325743+EdgarPerezArce@users.noreply.github.com>
2022-12-09 15:40:09 -0800

    Add files via upload

---
+++ Amazon.js
@@ -0,0 +1,30 @@
+console.log(`Script file`)
+const message =`Welcome to Amazon!`;
+const URL = `http://www/amazonPrivateEndPoint/project`;
+const taxes = 8.5;
+
+// user page info
+let userName = `John25`;
+let password = ``
+let age = 100;
+let phoneNumber = `813-455-6845`;
+let geolocation = `Tampa, FL`;
+let creditCard = `Visa`;
+let items = 100;
+//menu tab deals
+let epicDeals = `Flash sales`;
+let holidayGift = `christmas`;
+let comupters = `Aseus specials`
+let bestSellers = `Televisions`;
+let petSupplies = `Petsmart`;
+let Books = `Michu Kaku`;
+let outdoorRecreation = `Canoes`;
+// products
+let electronics = `Sony`;
+let fruniture = `lazy boy`;
+let appliances =`Ninja`;
+let makeup = `Maybelline`;
+let apparel = ``;
+let shoes = `Nike`;
+
+// page info contact info
