3ee898f92919efe6c04d16839e1670f644f3a0a3
EdgarPerezArce <edgarperezarce@gmail.com>
2022-12-12 18:27:53 -0800

    Update on amazon project 1
    Updated customer information and items also added template info

--- Amazon.js
+++ Amazon.js
@@ -12,26 +12,75 @@ let userName = `John25`;
 let password = `12345`;
 let age = 100;
 let phoneNumber = `813-455-6845`;
-let geolocation = `Tampa, FL`;
+let geoLocation = `Tampa, FL`;
 let creditCard = `Visa`;
 let items = 100;
 //menu tab deals
-let epicDeals = `Flash sales`;
-let holidayGift = `christmas`;
-let comupters = `Aseus specials`
+let epicDeals = `Flash sales!`;
+let holidayGift = `Christmas`;
+let computers = `Aseus specials`
 let bestSellers = `Televisions`;
 let petSupplies = `Petsmart`;
 let Books = `Michu Kaku`;
 let outdoorRecreation = `Canoes`;
 // products
 let electronics = `Sony`;
-let fruniture = `lazy boy`;
+let furniture = `lazy boy`;
 let appliances =`Ninja`;
 let makeup = `Maybelline`;
-let apparel = ``;
-let shoes = `Nike`;
+let apparel = `H&M`;
+let shoes = `Addis Yeezys`;
 
-// page info contact info
-console.log(userName,);
+// template string
+console.log(`
+    Web padge ${URL}
+    My Screenname ${userName}
+    The Password ${password}
+    My current age ${age} 
+    Phone number ${phoneNumber}
+    In ${geoLocation}
+    With ${creditCard}
+    Amount of Products ${items} 
+    We have great deals like ${epicDeals}
+    Ideas for ${holidayGift}
+    Computer devices ${computers}
+    Todays Best Sellers ${bestSellers}
+    All the Greatest pet supplies ${petSupplies}
+    BooksAmillion ${Books}
+    Out door advantures supplies ${outdoorRecreation}
+    The greatest electronics ${electronics}
+    Strong and durable ${furniture}
+    Quick and reliable ${appliances}
+    Beautiful rich makeup ${makeup}
+    Contreversially cool apparel ${apparel}
+    Hip brands on clearnce ${shoes}`);
 
-document.write(`Screenname ${ userName}`)
+document.write(`
+    <p> My Screenname ${userName}</p>
+    <p> The Password ${password}</p> 
+    <p> My current age ${age}</p> 
+    <p> Phone number ${phoneNumber}</p> 
+    <p> In ${geoLocation}</p> 
+    <p> With ${creditCard}</p>
+    <p> Amount of Products ${items}</p>
+    `);
+
+document.write(`
+    <h3> We have great deals like ${epicDeals}</h3>
+    <h3> Ideas for ${holidayGift}</h3> 
+    <h3> Computer devices ${computers}</h3>
+    <h3> Todays Best Sellers ${bestSellers}</h3>
+    <h3> All the Greatest pet supplies ${petSupplies}</h3>
+    <h3> BooksAmillion ${Books}</h3>
+    <h3> Out door advantures supplies ${outdoorRecreation}</h3>
+    `);
+
+document.write(`
+    <p> The greatest electronics ${electronics}</p>
+    <p> Strong and durable ${furniture}</p>
+    <p> Quick and reliable ${appliances}</p>
+    <p> Beautiful rich makeup ${makeup}</p>
+    <p> Contreversially cool apparel ${apparel}</p>
+    <p> Hip brands on clearnce ${shoes}</p> 
+    `);
+    
